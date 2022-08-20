7b6a8a028242094de63bfa822dff9ee1133d8f1a
malawadd <50057882+malawadd@users.noreply.github.com>
2022-08-20 23:38:14 +0300

    👍 setup project

--- next.config.js
+++ next.config.js
@@ -10,7 +10,17 @@ module.exports = {
     domain: ["ipfs.moralis.io"],
   },
   env:{
-    
+    NEXT_APP_MORALIS_SERVER_URL: process.env.NEXT_APP_MORALIS_SERVER_URL,
+    NEXT_APP_MORALIS_SERVER_PORT: process.env.NEXT_APP_MORALIS_SERVER_PORT,
+    NEXT_APP_WEB3_STORAGE_KEY : process.env.NEXT_APP_WEB3_STORAGE_KEY,
+    NEXT_APP_NFT_STORAGE_API_KEY : process.env.NEXT_APP_NFT_STORAGE_API_KEY,
+    NEXT_APP_WHEREBY_KEY : process.env.NEXT_APP_WHEREBY_KEY,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
   }
 
 }
